6b4b6c028c4ff0cc0cb8afb0b1154563bbf8e87d
westpain <homicide@disroot.org>
2024-08-12 19:49:29 +0300

    Drop obsolete architectures

--- app/src/main/java/com/termux/app/TermuxInstaller.java
+++ app/src/main/java/com/termux/app/TermuxInstaller.java
@@ -411,9 +411,7 @@ final class TermuxInstaller {
         for (String androidArch : Build.SUPPORTED_ABIS) {
             switch (androidArch) {
                 case "arm64-v8a": return "aarch64";
-                case "armeabi-v7a": return "arm";
                 case "x86_64": return "x86_64";
-                case "x86": return "i686";
             }
         }
         throw new RuntimeException("Unable to determine arch from Build.SUPPORTED_ABIS =  " +
